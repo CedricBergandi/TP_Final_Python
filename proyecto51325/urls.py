73b45ab3125a8f6d1612362ec245cc2405185073
Cedric Bergandi <bergandi.cedric@gmail.com>
2023-05-06 20:58:14 -0300

    Entrega final. Ajuste inicio

--- proyecto51325/urls.py
+++ proyecto51325/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", inicio),
+    path("", inicioApp),
     path("RiverPlate/", include("AppCoder.urls")),
     
 ]
